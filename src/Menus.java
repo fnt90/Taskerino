7bbe7ad76de26a4241b05501532a2c625ea8efc9
Fiona <thompsonfiona@outlook.com>
2021-03-05 18:40:17 +0100

    Added edit menu and edit submenu, added try/catch to avoid invalid menu inputs

--- src/Menus.java
+++ src/Menus.java
@@ -12,16 +12,21 @@ public class Menus {
         System.out.println("You entered: " + inputNumString);
 
         // how to disallow non int??
-        int inputNum = Integer.parseInt(inputNumString);
-        if (inputNum == 1) {
-            showMenu();
-        } else if (inputNum == 2) {
-            addMenu();
-        } else if (inputNum == 3) {
-            editMenu();
-        } else if (inputNum == 4) {
-            Messages.exitMsg();
-        } else {
+        try {
+            int inputNum = Integer.parseInt(inputNumString);
+            if (inputNum == 1) {
+                showMenu();
+             } else if (inputNum == 2) {
+                addMenu();
+            } else if (inputNum == 3) {
+                editMenu();
+            } else if (inputNum == 4) {
+                Messages.exitMsg();
+            } else {
+                Messages.invalidInputMsg();
+                mainMenu();
+            }
+        } catch(NumberFormatException e) {
             Messages.invalidInputMsg();
             mainMenu();
         }
@@ -33,12 +38,57 @@ public class Menus {
     }
 
     public static void addMenu() {
+        //here to display menu for task add
         Messages.addTasksMenuMsg();
     }
 
     public static void editMenu() {
         //here to display menu for edit
         Messages.editTasksMenuMsg();
+        System.out.println("Press enter to continue, imagining you are selecting a task from the list");
+
+        Scanner menuSelector = new Scanner(System.in);
+        menuSelector.nextLine();
+
+        editSubMenu();
+
     }
-    public static void
+    public static void editSubMenu() {
+        //this is the submenu for editing, after a task has been selected
+        Messages.editTasksSelectMsg();
+
+        Scanner menuSelector = new Scanner(System.in);
+        String inputNumString = menuSelector.nextLine();
+        System.out.println("You entered: " + inputNumString);
+
+        try {
+            int inputNum = Integer.parseInt(inputNumString);
+            if (inputNum == 1) {
+                //edit name
+            } else if (inputNum == 2) {
+                //edit due date
+            } else if (inputNum == 3) {
+                //edit project
+            } else if (inputNum == 4) {
+                //Messages.exitMsg();
+            } else if (inputNum == 5) {
+                //delete task
+            } else if (inputNum == 6) {
+                mainMenu();
+            } else {
+                Messages.invalidInputMsg();
+                editSubMenu();
+            }
+        } catch (NumberFormatException e) {
+            Messages.invalidInputMsg();
+            editSubMenu();
+        }
+
+    }
+    //This does not work as I want, inputNumString is left hanging
+    //public static void textPrompt() {
+       // Scanner menuSelector = new Scanner(System.in);
+       // String inputNumString = menuSelector.nextLine();
+       // System.out.println("You entered: " + inputNumString);
+   // }
 }
--- src/Messages.java
+++ src/Messages.java
@@ -31,13 +31,15 @@ public class Messages {
     }
     public static void editTasksMenuMsg() {
         //Message to let user know they entered Edit menu
+        System.out.println("EDIT MENU");
         System.out.println("You selected Edit, delete or tick off a task.");
         System.out.println("PRINT ALL TASKS HERE WITH ITERATOR NUMBER");
         System.out.println("Type the number of the task you want to select.");
     }
     public static void editTasksSelectMsg() {
         //Message to provide edit options after selecting a task to edit
-        System.out.println("What do you want to do?");
+        System.out.println("EDIT SUBMENU");
+        System.out.println("What do you want to do with this task?");
         System.out.println("1. Edit name");
         System.out.println("2. Edit due date");
         System.out.println("3. Edit project");
