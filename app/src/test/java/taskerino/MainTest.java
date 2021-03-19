ef01cd0e98f1f2ccd3cfe7dd7a7d59d77c4a3505
Fiona <thompsonfiona@outlook.com>
2021-03-19 09:27:20 +0100

    Create new class diagram with methods, fields, constructors

--- app/src/test/java/taskerino/MainTest.java
+++ app/src/test/java/taskerino/MainTest.java
@@ -1,7 +1,13 @@
 package taskerino;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-
+    //@Test void greetsUserAsBuddyIfBlankStringFound() {
+       // Main.main(args);
+       // assertEquals("Buddy", )
+        //
+   // }
 }
--- app/src/test/java/taskerino/MenusTest.java
+++ app/src/test/java/taskerino/MenusTest.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MenusTest {
-    @Test void mainMenuDisplaysCorrectly() {
+    @Test void showMenuReturnsToMainWhenTaskListEmpty() {
         Menus menus = new Menus();
-        //
+        menus.showMenu();
+        assertNull(menus.taskList);
     }
 
 }
